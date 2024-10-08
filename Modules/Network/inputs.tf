c1bc2c28103199d08a6094cd84562ef4a3367fb8
divya29 <ujjailydivya94@gmail.com>
2024-10-08 17:58:07 -0400

    created parent module for vnet

---
+++ Modules/Network/inputs.tf
@@ -0,0 +1,27 @@
+# Define Variables
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "primary_network_cidr" {
+  type        = list(string)
+  description = "This is primary networks cidr range"
+  default     = ["192.168.0.0/16"]
+}
+
+variable "subnet_names" {
+  type        = list(string)
+  description = "subnet names"
+  default     = ["web", "app", "db"]
+}
+
+variable "subnet_cidrs" {
+  type        = list(string)
+  description = "subnet cidrs"
+  default     = ["192.168.0.0/24", "192.168.1.0/24", "192.168.2.0/24"]
+}

ff6444d8c57a3582877e222b3f8cefd015464717
divya29 <ujjailydivya94@gmail.com>
2024-10-08 17:05:27 -0400

    separating network and nsg

--- Module/main.tf
+++ Module/main.tf
@@ -22,3 +22,18 @@ resource "azurerm_virtual_network" "main" {
   }
   depends_on = [azurerm_resource_group.group]
 }
+
+# creating multiple subnets
+
+resource "azurerm_subnet" "subnets" {
+  count                = length(var.subnet_names)
+  name                 = var.subnet_names[count.index]
+  resource_group_name  = azurerm_resource_group.group.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = [var.subnet_cidrs[count.index]]
+  depends_on = [
+    azurerm_resource_group.group,
+    azurerm_virtual_network.main
+  ]
+
+}

2c9c6f0e0eaed72c2ebb558954e7cbd672e90e1d
divya29 <ujjailydivya94@gmail.com>
2024-10-08 17:32:36 -0400

    separated net and nsg

--- Module/Vnet/inputs.tf
+++ Module/Vnet/inputs.tf
@@ -24,6 +24,33 @@ variable "subnet_cidrs" {
   default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
+variable "web_nsg_rules" {
+  type = list(object({
+    name                       = string
+    description                = string
+    protocol                   = string
+    source_port_range          = string
+    destination_port_range     = string
+    source_address_prefix      = string
+    destination_address_prefix = string
+    access                     = string
+    priority                   = number
+    }
+
+  ))
+  description = "web nsg rules"
+  default = [{
+    name                       = "openhttp"
+    description                = "this opens 80 port"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+    access                     = "Allow"
+    priority                   = 1000
+  }]
+}
 
 
 

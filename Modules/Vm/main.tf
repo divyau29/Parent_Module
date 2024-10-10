ad607bbd5e4083bf6ce48df4a584e51ee6c083cf
divya29 <ujjailydivya94@gmail.com>
2024-10-10 09:49:15 -0400

    added vm ubuntu and public ip module

--- Modules/Vm/main.tf
+++ Modules/Vm/main.tf
@@ -1,5 +1,14 @@
 # Virtual Machine Module: main.tf
 
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = "${var.vm_name}-public-ip"
+  resource_group_name  = var.resource_group_name
+  location            = var.location
+  allocation_method   = "Dynamic"
+  sku                  = "Basic"
+}
+
 resource "azurerm_network_interface" "nic" {
   name                = "${var.vm_name}-nic"
   resource_group_name  = var.resource_group_name
@@ -9,6 +18,7 @@ resource "azurerm_network_interface" "nic" {
     name                          = "${var.vm_name}-ipconfig"
     subnet_id                    = var.subnet_id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id         = azurerm_public_ip.public_ip.id
   }
 }
 
@@ -34,8 +44,20 @@ resource "azurerm_virtual_machine" "vm" {
     admin_password = var.vm_admin_password
   }
 
-  os_profile_windows_config {
-    provision_vm_agent = true
+  # os_profile_windows_config {
+  #   provision_vm_agent = true
+  # }
+
+   os_profile_linux_config {
+    disable_password_authentication = false  # Allows password authentication for Linux
+  }
+
+  # Reference to Ubuntu image in Azure marketplace
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "20.04-LTS"  # You can change to 22.04-LTS or any other supported version
+    version   = "latest"
   }
 
   tags = {
